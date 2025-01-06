647f912f0edfe69d42fe81b1a8cd9065371d33ec
Habiba <habibaswaify@hotmail.com>
2025-01-06 23:57:11 +0200

    Add INotifier

---
+++ src/main/java/org/SOLID/OCP/INotifier.java
@@ -0,0 +1,5 @@
+package org.SOLID.OCP;
+// Open/Closed Principle Application: Different classes for notifications that implement the same interface
+public interface INotifier {
+        void notifyCustomer();
+}
