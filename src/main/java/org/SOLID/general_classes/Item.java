8c3625b09e53d0227519dfff95e22a553a9c2d4a
Habiba <habibaswaify@hotmail.com>
2025-01-07 00:11:06 +0200

    Add Item

---
+++ src/main/java/org/SOLID/general_classes/Item.java
@@ -0,0 +1,28 @@
+package org.SOLID.general_classes;
+
+public class Item {
+    private String itemName;
+    private double itemPrice;
+
+    public Item(String itemName, double itemPrice) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+}
